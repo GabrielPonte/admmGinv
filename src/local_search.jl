3432dc2c6b6cc41b8468f3293c29b10fd7e06814
Gabriel Ponte <gabrielponte@poli.ufrj.br>
2024-05-27 09:37:07 -0400

    fast 2,1-norm

--- src/local_search.jl
+++ src/local_search.jl
@@ -1,8 +1,19 @@
-using LinearAlgebra
+getLICols(A,r) = (qr(A, ColumnNorm()).p)[1:r]
+getLIRows(A,r) = (qr(A', ColumnNorm()).p)[1:r]
 
-include("types.jl")
-include("util.jl")
-
+function get_LI_RC(A,r)
+    time_start = time_ns() 
+    C = getLICols(A,r);
+    time_start2 = time_ns()
+    time_2 =  (time_start2 - time_start)/1e9
+    println("Found columns in $(round(time_2,digits=3)) sec.")
+    R = getLIRows(A[:,C],r);
+    time_3 =  (time_ns() - time_start2)/1e9
+    println("Found rows in $(round(time_3,digits=3)) sec.")
+    elapsed_time = (time_ns() - time_start)/1e9
+    return R,C,elapsed_time
+end
+    
 function lu_rank_one_update(L, U, y, z)
     # only works for P = (1:n) otherwise y = y[P]
     # z = ei
@@ -85,24 +96,30 @@ function LS_det(A,R,C,TP::Symbol)
     H_hat = (A_hat'*A_hat) \ A_hat';
     H = zeros(n,m);
     H[C,:] .= H_hat;
-    return H,time_ls,swaps,det_Ar
+    return H,time_ls,swaps,det_Ar,C
 end
 
 function LS_21(A,C,TP::Symbol)
     time_start = time_ns()
     m,n = size(A); r = length(C);
     swaps = 0;
-    H_hat = pinv(A[:,C]);
+    # H_hat = pinv(A[:,C]);
+    A_hat =  A[:,C];
+    H_hat = (A_hat'*A_hat) \ A_hat';
     norm21 = getnorm21(H_hat);
     Cb = setdiff((1:n),C);
+    Psi = H_hat*A[:,Cb];
     i_save,j_save = 0,0; 
     vbar_save,vj_save = Nothing,0;
+    norm_Hj_save = 0.0;
+    W = H_hat*H_hat'
+    norm_H_hat_row = norm.(eachrow(H_hat)).^2;
     flag = true;
     while flag
         flag = false;
-        for (col_i,Cb_i) in enumerate(Cb)
+        for col_i in (1:n-r)
             # Get vector v to efficiently compute new 21-norm
-            v = H_hat*A[:,Cb_i];
+            v = Psi[:,col_i] #H_hat*A[:,Cb_i];
             Sj = collect(2:r); # remove j from (1:r)
             for j = (1:r)
                 if j >= 2
@@ -113,17 +130,23 @@ function LS_21(A,C,TP::Symbol)
                     continue;
                 end
                 v_bar = -v[Sj]./vj;
-                Hs = H_hat[Sj,:];
-                Hj = H_hat[j,:];
-                norm_Hj = norm(Hj,2);
-                w = Hs*Hj;
+                # Hs = H_hat[Sj,:];
+                # Hj = (H_hat[j,:]);
+                
+                # w = Hs*Hj; # W[Sj,j]
+                # w =  W[Sj,j];
+                # @show norm(w - W[Sj,j])
+
+                norm_Hj = sqrt(norm_H_hat_row[j]) #norm(H_hat[j,:],2);
+                
                 new_21norm  = norm_Hj/abs(vj);
-                new_21norm += sum(sqrt.(norm.(eachrow(Hs)).^2 +  2*v_bar.*w +  v_bar.^2*norm_Hj^2 )); 
+                new_21norm += sum(sqrt.(norm_H_hat_row[Sj] +  2*v_bar.*W[Sj,j] +  v_bar.^2*norm_Hj^2 )); 
                 if new_21norm < norm21
                     flag = true; 
                     norm21 = new_21norm;
                     i_save,j_save = col_i,j;
                     vbar_save = v_bar;
+                    norm_Hj_save = norm_Hj;
                     vj_save = vj;
                     if TP == :FI
                         break  
@@ -145,27 +168,73 @@ function LS_21(A,C,TP::Symbol)
             H_hat[j,:] .= (1/vj_save)*H_hat[j,:];
             # update columns
             C[j],Cb[i] = Cb[i],C[j]; 
-            # v1 = isapprox(abs(norm21-getnorm21(pinv(A[:,C]))),0;atol=1e-6)
-            # v2= isapprox(norm(pinv(A[:,C])-H_hat),0;atol=1e-6)
-            # @show v1,v2
+            # update W
+            Y = W[Sj,j]*vbar_save';
+            W[Sj,Sj] += (vbar_save*vbar_save')*norm_Hj_save^2 + Y + Y';
+            W[Sj,j] = (1/vj_save)*(W[Sj,j] + (norm_Hj_save^2)*vbar_save)
+            W[j,Sj] =  W[Sj,j];
+            W[j,j] = (norm_Hj_save/vj_save)^2
+            # @show norm(W - H_hat*H_hat')
+            # update Psi
+            # @show size(Psi[Sj,:])
+
+            Psi[Sj,:] += vbar_save.*Psi[j,:]';
+            Psi[j,:] *= (1/vj_save);
+            # @show size(A[:,Cb[i]])
+            # @show size(H_hat*A[:,Cb[i]])
+            Psi[:,i] .= H_hat*A[:,Cb[i]]; 
+            # update norm H hat squared
+            norm_H_hat_row = norm.(eachrow(H_hat)).^2;
+            # @show norm(Psi -  H_hat*A[:,Cb])
+            # display("text/plain", W- H_hat*H_hat')
+            # return
         end
     end
     # get output
-    time_ls = (time_ns() - time_start)/1e9;
-    det_Ar = det(A[R,C]);  
+    time_ls21 = (time_ns() - time_start)/1e9;
+    # det_Ar = det(A[R,C]);  
     A_hat =  A[:,C];
     H_hat = (A_hat'*A_hat) \ A_hat';
     H = zeros(n,m);
     H[C,:] .= H_hat;
-    return H,time_ls,swaps,det_Ar
+    # @show getnorm21(H),norm21
+    # @show time_ls21
+    return H,time_ls21,swaps
 end
 
-m,n,r = 30,20,5;
-A = rand(m,r)*rand(r,n);
-R = (1:r); C = collect(1:r);
-H,time_ls,swaps,det_FI = LS_det(A,R,C,:FI);
-H,time_ls,swaps,det_FP = LS_det(A,R,C,:FP);
-H,time_ls,swaps,det_BI = LS_det(A,R,C,:BI);
-LS_21(A,C,:BI);
-1;
+function genPlotDetvs21()
+    m =2000;
+    n,r = floor(Int64,1.0*m),floor(Int64,0.1*m);
+    nameInst = string("A_",m,"_",n,"_",r);
+    println(string("\nStarting instance: m,n,r: ",m,",",n,",",r))
+    A = getMatlabInstance(nameInst,"A");
+    R_init,C_init,time_RC = get_LI_RC(A,r)
+    min_21n = Inf
+    res = []
+    for type_det in [:FI,:FP,:BI]
+        H,time_det,swaps_det,det_Ar,C = LS_det(A,copy(R_init),copy(C_init),type_det)
+        n21old = getnorm21(H);
+        H,time_ls21,swaps = LS_21(A,C,:FI);
+        tdet = time_RC+time_det;
+        n21new = round(getnorm21(H),digits=3);
+        if n21new < min_21n
+            min_21n = n21new
+        end
+        t21 = tdet + time_ls21
+        push!(res,[type_det,n21old,n21new,tdet,t21,0,0]);
+    end
+    for i = (1:3)
+        res[i][6] = (res[i][2] - min_21n)/min_21n
+        res[i][7] = (res[i][3] - min_21n)/min_21n
+    end
+    return res
+end
+# m,n,r = 30,20,5;
+# A = rand(m,r)*rand(r,n);
+# R = (1:r); C = collect(1:r);
+# H,time_ls,swaps,det_FI = LS_det(A,R,C,:FI);
+# H,time_ls,swaps,det_FP = LS_det(A,R,C,:FP);
+# H,time_ls,swaps,det_BI = LS_det(A,R,C,:BI);
+# LS_21(A,C,:BI);
+# 1;
 # det_FI,det_FP,det_BI
--- src/main.jl
+++ src/main.jl
@@ -5,10 +5,12 @@ using MAT,DelimitedFiles
 import MutableArithmetics
 const MA = MutableArithmetics
 const SA = MA.SparseArrays
+using Distributed
 
 using JLD
 using Plots, LaTeXStrings
 
+using Profile
 
 include("types.jl")
 include("util.jl")
@@ -16,7 +18,8 @@ include("instances.jl")
 include("closed_form_sols.jl")
 include("admm.jl")
 include("step_sizes.jl")
-include("solvers.jl")
+# include("solvers.jl")
+include("local_search.jl")
 
 arr_resADMM_1,arr_resADMM_0 = init_ginv_res_admm(),init_ginv_res_admm()
 arr_resADMM_21,arr_resADMM_20,arr_resADMM_210 = init_ginv_res_admm(),init_ginv_res_admm(),init_ginv_res_admm();
@@ -28,21 +31,38 @@ TP = :ML
 ω21 = 0.5
 #arr_res_all = [];
 
+
+
+
 global m,n,r  = 100,200,70;
 M = [100,200,300,400,500,1000,2000,3000,4000,5000];
 
 pres,dres,tols,objs,rhos = [],[],[],[],[];
 
-for m1 in [1000,2000,3000,4000,5000]
-    m = m1;
-    n,r = floor(Int64,0.5*m),floor(Int64,0.25*m);
-    nameInst = string("A_",m,"_",n,"_",r);
-    println(string("\nStarting instance: m,n,r: ",m,",",n,",",r))
-    A = getMatlabInstance(nameInst,"A");
+# for m1 in [200]#[1000,2000,3000,4000,5000]
+m1 = 1000;
+m = m1;
+n,r = floor(Int64,0.5*m),floor(Int64,0.25*m);
+nameInst = string("A_",m,"_",n,"_",r);
+println(string("\nStarting instance: m,n,r: ",m,",",n,",",r))
+A = getMatlabInstance(nameInst,"A");
+R,C,time_RC = get_LI_RC(A,r)
+H,time_det,swaps_det,det_Ar,C = LS_det(A,R,C,:FP)
+n21old = round(getnorm21(H),digits=3)
+@show time_det
+# BLAS.set_num_threads(1) # 1 is better than 32 (?)
+# println("32")
+@profview LS_21(A,C,:FI);
+# H,time_ls21,swaps = LS_21(A,C,:FI);
+# swaps
+# n21new = round(getnorm21(H),digits=3)
+# n21old,n21new
+    # @show swaps_det,swaps_21
+    # @show rank(A[R,C],1e-6),r
     # global A = rand(m,r) * rand(r,n)
-    inst = GinvInst(A,m,n,r);
+    # inst = GinvInst(A,m,n,r);
     # Initialization
-    ginvInit = getInitialInfoGinv(inst)
+    # ginvInit = getInitialInfoGinv(inst)
 
     # time_admm_1 = @elapsed admmsol_1 = admm1norm_v2(ginvInit,eps_opt=1e-4,stop_limit=:OptGap);
     # admmsol_1.z = getnorm1(admmsol_1.H);
@@ -61,13 +81,13 @@ for m1 in [1000,2000,3000,4000,5000]
     # println(string("m = ",m,". BREG 1 finished in ", round_exact(bregsol_1.time,2), " sec. 1 norm ", round_exact(bregsol_1.z,3), ", 0 norm ", bregres_1.norm_0, ". Iter: ", bregsol_1.iter));
     # flush(stdout)
     
-    time_admm_21 = @elapsed admmsol_21,M_21,Λ_21 = admm21norm(ginvInit);#runADMM21n(V1Dinv,V2,U1,Λ21,TP,false);
-    admmsol_21.z = getnorm21(admmsol_21.H);
-    admmsol_21.time = time_admm_21;
-    admmres_21 = getResultsADMM(inst,admmsol_21);
-    writeCSV!(arr_resADMM_21,admmres_21,Symbol(:ADMM_21_,TP))
-    println(string("m = ",m,". ADMM 2,1: ", round_exact(admmsol_21.time,2), " sec. |2,1|: ", round_exact(admmsol_21.z,3), ". |2,0|: ", admmres_21.NZR, ". |0|: ", admmres_21.norm_0,". Iter: ", admmsol_21.iter));
-    flush(stdout)
+    # time_admm_21 = @elapsed admmsol_21,M_21,Λ_21 = admm21norm(ginvInit);#runADMM21n(V1Dinv,V2,U1,Λ21,TP,false);
+    # admmsol_21.z = getnorm21(admmsol_21.H);
+    # admmsol_21.time = time_admm_21;
+    # admmres_21 = getResultsADMM(inst,admmsol_21);
+    # writeCSV!(arr_resADMM_21,admmres_21,Symbol(:ADMM_21_,TP))
+    # println(string("m = ",m,". ADMM 2,1: ", round_exact(admmsol_21.time,2), " sec. |2,1|: ", round_exact(admmsol_21.z,3), ". |2,0|: ", admmres_21.NZR, ". |0|: ", admmres_21.norm_0,". Iter: ", admmsol_21.iter));
+    # flush(stdout)
 
     # time_admm_0 = @elapsed admmsol_0 = admm0norm(ginvInit,trunc(Int64,m*(n-2)));
     # admmsol_0.z = getnorm1(admmsol_0.H);
@@ -78,20 +98,20 @@ for m1 in [1000,2000,3000,4000,5000]
     # println(string("m = ",m,". ADMM 0 finished in ", round_exact(admmsol_0.time,2), " sec. 1 norm ", round_exact(admmsol_0.z,3), ", 0 norm ", admmres_0.norm_0, ". Iter: ", admmsol_0.iter));
     # flush(stdout)
 
-    """
-    0.70 = 1e2 
-    0.90 = 1e3
-    0.95 = 1e4
+    # """
+    # 0.70 = 1e2 
+    # 0.90 = 1e3
+    # 0.95 = 1e4
 
-    """
-    ω21 = 0.95; nzr21 = admmres_21.NZR;
-    time_admm_20 = @elapsed admmsol_20 = admm20norm(ginvInit,ω21,nzr21,M_21,Λ_21;rho=1e3);#runADMM20n(V1Dinv,V2,U1,admmres_21.NZR,ω21,false);
-    admmsol_20.z = getnorm21(admmsol_20.H);
-    admmsol_20.time = time_admm_20;
-    admmres_20 = getResultsADMM(inst,admmsol_20);
-    writeCSV!(arr_resADMM_20,admmres_20,Symbol(:ADMM_2120_,trunc(Int64,100*ω21)))
-    println(string("m = ",m,". ADMM 2,0: ", round_exact(admmsol_20.time,2), " sec. |2,1|: ", round_exact(admmsol_20.z,3), ". |2,0|: ", admmres_20.NZR,". |0|: ", admmres_20.norm_0,". Iter: ", admmsol_20.iter));
-    flush(stdout)
+    # """
+    # ω21 = 0.95; nzr21 = admmres_21.NZR;
+    # time_admm_20 = @elapsed admmsol_20 = admm20norm(ginvInit,ω21,nzr21,M_21,Λ_21;rho=1e3);#runADMM20n(V1Dinv,V2,U1,admmres_21.NZR,ω21,false);
+    # admmsol_20.z = getnorm21(admmsol_20.H);
+    # admmsol_20.time = time_admm_20;
+    # admmres_20 = getResultsADMM(inst,admmsol_20);
+    # writeCSV!(arr_resADMM_20,admmres_20,Symbol(:ADMM_2120_,trunc(Int64,100*ω21)))
+    # println(string("m = ",m,". ADMM 2,0: ", round_exact(admmsol_20.time,2), " sec. |2,1|: ", round_exact(admmsol_20.z,3), ". |2,0|: ", admmres_20.NZR,". |0|: ", admmres_20.norm_0,". Iter: ", admmsol_20.iter));
+    # flush(stdout)
 
     # time_admm_210 = @elapsed admmsol_210 = runADMM210n(V1Dinv,V2,U1,Λ21,floor(0.75*n));
     # admmsol_210.z = getnorm21(admmsol_210.H);
@@ -122,7 +142,8 @@ for m1 in [1000,2000,3000,4000,5000]
     
 
     #writeAllCSV!(arr_res_all,mskres,grbres,admmres);
-end
+# end
+
 # num_init = 1
 # plot(It,[P1,D1],labels=[L"\mathcal{P}" L"\mathcal{D}"], yaxis=:log, yticks =  10.0.^(5:-1:-15),xlabel = L"k" ,ylabel="residual")
 # plot(It,D1,label=string(ω21), xlabel =L"k",ylabel=L"{\|\|H\|\|}_{2,0}")
